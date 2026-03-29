a2e152625b58773408b94d013b32a04ccec8f7e8
Doan Viet Khang <doanvankhoa2011@gmail.com>
2026-03-29 16:05:01 +0700

    initial commit

--- src/Cau12.java
+++ src/Cau12.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class Cau12 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Nhập đường dẫn đến tệp cần kiểm tra (VD:\"C:\\Users\\Khang\\Downloads\\data.txt\"): ");
+        System.out.print("Nhập đường dẫn đến tệp cần kiểm tra (VD:C:\\Users\\Khang\\Downloads\\data.txt): ");
         String duongDan = scanner.nextLine();
         File file = new File(duongDan);
 
